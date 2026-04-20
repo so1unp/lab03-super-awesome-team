7ee5a5c2fbec8a1d689368eb9b340fa13827e7e6
lohse.agustin <lohse.agustin@gmail.com>
2026-04-20 18:42:27 -0300

    Desarrollo exec case

--- sh.c
+++ sh.c
@@ -66,14 +66,24 @@ void runcmd(struct cmd *cmd)
             ecmd = (struct execcmd *) cmd;
             if (ecmd->argv[0] == 0)
                 exit(0);
-            fprintf(stderr, "exec not implemented\n");
-            // Your code here ...
-            break;
+            execvp(ecmd->argv[0], ecmd->argv);
+            perror(ecmd->argv[0]);
+            exit(-1);
 
         case REDIR:
-            fprintf(stderr, "redir not implemented\n");
-            // Your code here ...
             rcmd = (struct redircmd *) cmd;
+            {
+                int fd = open(rcmd->file, rcmd->mode, 0644);
+                if (fd < 0) {
+                    perror(rcmd->file);
+                    exit(-1);
+                }
+                if (dup2(fd, rcmd->fd) < 0) {
+                    perror("dup2");
+                    exit(-1);
+                }
+                close(fd);
+            }
             runcmd(rcmd->cmd);
             break;
 
